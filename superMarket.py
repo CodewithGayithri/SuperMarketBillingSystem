a0d22ce41359ce93b8e3608da96136b99c80c117
gayithri <ngayithri22@gmail.com>
2026-03-24 21:41:19 +0530

    firdt commit

--- superMarket.py
+++ superMarket.py
@@ -15,6 +15,7 @@ finalPrice = 0
 ilist=[]
 iprice = []
 qlist = []
+plist = []
 
 items = {"Rice":50,
 "Maggie": 15,
@@ -31,7 +32,39 @@ for i in range(len(items)):
         quantity = int(input("Enter The Quantity: "))
         if item in items.keys():
             price = quantity * (items[item])
-print(price)
+            pricelist.append((item,quantity,items,price))
+            totalprice += price
+            ilist.append(item)
+            qlist.append(quantity)
+            plist.append(price)
+            gst = (totalprice*5)/100
+            finalAmount = gst + totalprice
+        else:
+            print("Sorry Item is Not Available")
+    else:
+        print("You Entered Wrong Number")
+    inp1 = input("Doy Want Bill yes OR no")
+    if inp1 == 'yes':
+        pass
+        if finalAmount!=0:
+            print(25*"=","Super Market",25*"=")
+            print(28*" ","Banglore")
+            print("Name:",name,30*" ","Date: ",datetime.now())
+            print(75*"-")
+            print("sno",8*" ",'items',8*" ","Quantity",3*" "
+                  ,"Price",)
+            for i in range(len(pricelist)):
+                print(i,10*" ",ilist[i],12*" ",
+                      qlist[i],8*" ",plist[i])
+            print(75*"-")
+            print(45*" ","TotalAmount: ","Rs",totalprice)
+            print(45*" ","GSTAmount: ","Rs",gst)
+            print(75*"-")
+            print(50*" ","FinalAmount: ","Rs",finalAmount)
+            print(75*"-")
+            print("Thank You")
+    
+
 
 
     
